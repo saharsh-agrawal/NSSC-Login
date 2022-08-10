d6461bca04f44cb673df45d20c0afe21b9f22c81
dev-064 <98254932+dev-064@users.noreply.github.com>
2022-08-10 20:51:11 +0530

    Add files via upload

---
+++ Saharsh/know more/main.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    setTimeout(function(){
+        const loader = document.getElementById('loader');
+        loader.style.display = 'none';
+        loader.style.zIndex = '-2000';
+    },3000);
+}
